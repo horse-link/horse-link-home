e3b4dc423374f6fa4e40d8c06327d1b2c87514e9
Peerawas Archavanuntakun <peerawas.archavanuntakun@dltx.io>
2023-03-07 09:57:48 +1000

    mapped expected error

--- src/pages/Social.tsx
+++ src/pages/Social.tsx
@@ -7,6 +7,21 @@ import { Horse } from "../assets";
 import { Link } from "react-router-dom";
 import constants from "../constants";
 
+const TWEET_MUST_INCLUDE_HL_TEXT =
+  "Your tweet must include the word “horse.link” to register for the competition";
+
+const ALREADY_REGISTERED_TEXT =
+  "You have already successfully registered for the competition";
+const errorMapping: { [key: string]: string } = {
+  "Tweet content does not contain any links": TWEET_MUST_INCLUDE_HL_TEXT,
+  'Tweet content does not contain "horse.link"': TWEET_MUST_INCLUDE_HL_TEXT,
+  "Invalid tweet url": TWEET_MUST_INCLUDE_HL_TEXT,
+  "Could not get oembed data":
+    "Could not load the tweet, make sure that the tweet is public",
+  "User already exists": ALREADY_REGISTERED_TEXT,
+  "Wallet already exists": ALREADY_REGISTERED_TEXT
+};
+
 const Social: React.FC = () => {
   const [walletAddress, setWalletAddress] = useState("");
   const [tweetUrl, setTweetUrl] = useState("");
@@ -35,15 +50,25 @@ const Social: React.FC = () => {
       setMsg(res);
     } catch (e) {
       console.error(e);
-      if (e instanceof AxiosError) {
-        setError(e?.response?.data?.details || e?.request || e?.message);
-      } else if (e instanceof Error) {
-        setError(e.message);
-      } else {
-        setError("Something went wrong");
-      }
+      handleError(e);
     } finally {
       setLoading(false);
+    }
+  };
+
+  const handleError = (e: any) => {
+    if (e instanceof AxiosError) {
+      const expectedError = e?.response?.data?.details;
+      if (expectedError) {
+        const mappedErrorMessage = errorMapping[expectedError];
+        setError(mappedErrorMessage);
+      } else {
+        setError(e?.request || e?.message);
+      }
+    } else if (e instanceof Error) {
+      setError(e.message);
+    } else {
+      setError("Something went wrong");
     }
   };
 
