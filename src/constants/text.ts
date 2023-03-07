c30c65d619a1d5bbf2497f9d26ae162dc766b1c7
Peerawas Archavanuntakun <peerawas.archavanuntakun@dltx.io>
2023-03-07 11:25:01 +1000

    adjust text for the tweet

--- src/constants/text.ts
+++ src/constants/text.ts
@@ -1,2 +1,2 @@
 export const TWEET =
-  "Hey, I've just entered into this competition with https://horse.link where I can go in the draw to win 0.2 Bitcoin. Register here: https://horse.link to enter.";
+  "GM, I've just entered into a competition with horse.link to win 0.2 Bitcoin. Register at horse.link to enter.";
