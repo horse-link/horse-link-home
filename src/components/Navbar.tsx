f5fc42a89cfa4fff10f44341648fa52f2afe03e2
C00p3rb123 <48899363+C00p3rb123@users.noreply.github.com>
2023-03-08 15:09:59 +1000

    removing target

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 import { Button } from "./Button";
 
 export const Navbar: React.FC = () => (
-  <div className="fixed top-0 w-full bg-white flex items-center justify-between p-6 shadow-xl">
-    <Link className="w-full flex items-center gap-x-3" to="/">
+  <div className="fixed top-0 flex w-full items-center justify-between bg-white p-6 shadow-xl">
+    <Link className="flex w-full items-center gap-x-3" to="/">
       <img
         src={Horse}
         alt="Horse Link logo"
-        className="h-[2.4rem] hidden lg:block"
+        className="hidden h-[2.4rem] lg:block"
       />
       <h2 className="text-sm lg:text-xl">Horse : Link</h2>
     </Link>
