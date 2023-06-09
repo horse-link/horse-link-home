648d3344181f353af12681bc2a039d83db9b1c5a
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-06-09 11:54:24 +1000

    chore: bars

--- src/components/homepage/GrayBackground.tsx
+++ src/components/homepage/GrayBackground.tsx
@@ -6,16 +6,19 @@ type Props = {
 
 export const GrayBackground: React.FC<Props> = ({ children }) => (
   <div className="relative h-full min-h-screen w-screen max-w-[100vw] bg-hl-home-background text-white">
-    <div className="absolute inset-0 z-0 flex h-full w-full divide-x-2 divide-hl-home-background-bars">
-      <div className="h-full w-full" />
-      <div className="h-full w-full" />
-      <div className="h-full w-full" />
-      <div className="h-full w-full" />
-      <div className="h-full w-full" />
-      <div className="h-full w-full" />
-      <div className="h-full w-full" />
-      <div className="h-full w-full" />
+    {/* bars */}
+    <div className="absolute inset-0 z-0 flex h-full w-full divide-x-[1px] divide-hl-home-background-bars">
+      <div className="h-full w-[6.25%] opacity-[30%]" />
+      <div className="h-full w-[12.5%] opacity-[30%]" />
+      <div className="h-full w-1/6 opacity-[30%]" />
+      <div className="h-full w-1/4 opacity-[30%]" />
+      <div className="h-full w-1/4 opacity-[30%]" />
+      <div className="h-full w-1/6 opacity-[30%]" />
+      <div className="h-full w-[12.5%] opacity-[30%]" />
+      <div className="h-full w-[6.25%] opacity-[30%]" />
     </div>
+
+    {/* rest of page */}
     <div className="absolute inset-0 z-50 h-full w-full">{children}</div>
   </div>
 );
