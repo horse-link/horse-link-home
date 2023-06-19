d4643e325177571a992b63f5c4579c4b7da73ab7
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2023-06-20 09:59:49 +1000

    feat: external

--- src/components/homepage/Header.tsx
+++ src/components/homepage/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { FunkyLink } from "./FunkyLink";
+import { Link } from "react-router-dom";
 
 export const Header: React.FC = () => (
   <header className="fixed top-0 z-[100] flex w-full items-center justify-between py-10 px-14">
-    <h2 className="font-basement text-3xl">
+    <Link to="/" className="block font-basement text-3xl">
       HORSE<span className="text-hl-secondary">LINK</span>
-    </h2>
-    <FunkyLink to="/" small>
+    </Link>
+    <FunkyLink to="https://alpha.horse.link" small>
       LAUNCH APP
     </FunkyLink>
   </header>
